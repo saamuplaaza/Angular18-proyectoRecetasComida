56deef884e8123ed677a28783371d5bc2613bab7
Samuel Plaza <samuelplazasaez@gmail.com>
2024-10-31 12:49:25 +0100

    enrutamiento creado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'landing', component: LandingComponent},
+    {path:'login', loadComponent: () => import('./pages/login/login.component').then(m=>m.LoginComponent)},
+    {path:'home/:type', loadComponent: () => import('./pages/home/home.component').then(m=>m.HomeComponent)},
+    {path:'home', redirectTo:'/home/category', pathMatch:'full'},
+    {path:'recipes/:type/:ingredient', loadComponent: ()=> import('./pages/list-recipes/list-recipes.component').then(m=>m.ListRecipesComponent)},
+    {path:'recipe/:id', loadComponent: ()=> import('./pages/view-recipe/view-recipe.component').then(m=>m.ViewRecipeComponent)},
+    {path:'', redirectTo:'/landing', pathMatch:'full'},
+    {path:'**', component: ErrorPageComponent}
+];
---
+++ src/app/pages/list-recipes/list-recipes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-recipes',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-recipes.component.html',
+  styleUrl: './list-recipes.component.css'
+})
+export class ListRecipesComponent {
+
+}
